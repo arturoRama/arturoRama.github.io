779f35301f8693f7a38953cb05c24c3f47e14770
Arturo Ramirez <GRamirez@amige.com.mx>
2024-08-14 15:14:58 -0700

    Changed the way the timer works, reset and pause work

--- script.js
+++ script.js
@@ -1,153 +1,5 @@
-// (function () {
-//     const milliseconds = 1,
-//           second = milliseconds * 1000,
-//           minute = second * 60,
-//           hour = minute * 60,
-//           day = hour * 24;
-  
-//     //I'm adding this section so I don't have to keep updating this pen every year :-)
-//     //remove this if you don't need it
-//     let today = new Date(),
-//         dd = String(today.getDate()).padStart(2, "0"),
-//         mm = String(today.getMonth() + 1).padStart(2, "0"),
-//         yyyy = today.getFullYear(),
-//         nextYear = yyyy + 1,
-//         dayMonth = "09/27/",
-//         birthday = dayMonth + yyyy;
-    
-//     today = mm + "/" + dd + "/" + yyyy;
-//     if (today > birthday) {
-//       birthday = dayMonth + nextYear;
-//     }
-//     //end
-    
-//     const countDown = new Date(birthday).getTime(),
-//         x = setInterval(function() {    
-  
-//           const now = new Date().getTime(),
-//                 distance = countDown - now;
-  
-//           // document.getElementById("days").innerText = Math.floor(distance / (day)),
-//           //   document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-//           //   document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-//           //   document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-  
-//           //do something later when date is reached
-//           if (distance < 0) {
-//             document.getElementById("headline").innerText = "It's my birthday!";
-//             document.getElementById("countdown").style.display = "none";
-//             document.getElementById("content").style.display = "block";
-//             clearInterval(x);
-//           }
-//           //seconds
-//         }, 0)
-//   }
-//   ()
-// );
-
-function startTimer(){
-  const milliseconds = 1,
-          second = milliseconds * 1000,
-          minute = second * 60,
-          hour = minute * 60,
-
-	inhale1 = document.getElementById("inhale1").value;
-	exhale1 = document.getElementById("exhale1").value;
-	inhale2 = document.getElementById("inhale2").value;
-	exhale2 = document.getElementById("exhale2").value;
-	inhale3 = document.getElementById("inhale3").value;
-	exhale3 = document.getElementById("exhale3").value;
-	hipopresive = document.getElementById("hipopresive").value;
-	totalTimer = document.getElementById("timer").value;
-
-  const intervals = [
-    inhale1,
-    exhale1,
-    inhale2,
-    exhale2,
-    inhale3,
-    exhale3,
-    hipopresive
-  ];
-  
-  let i = 0;
-  let now = new Date();
-  let firstBeep = new Date();
-  let nextBeep = firstBeep.setSeconds(firstBeep.getSeconds() + Number(intervals[i++]));
-  const timer = addMinutes(now, Number(totalTimer));
-  const x = setInterval(function() {    
-
-          now = new Date();
-          firstBeep = new Date();
-          let distance = timer - now;
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-          let distance2 = nextBeep - firstBeep
-          //do something later when date is reached
-          if (distance2 <= 0) {
-            nextBeep = firstBeep.setSeconds(firstBeep.getSeconds() + Number(intervals[i++]));
-            i = i % intervals.length;
-            beep('500', '450', '0.8', 'sine');
-          }
-
-          if (distance < 0) {
-            document.getElementById("headline").innerText = "It's my birthday!";
-            document.getElementById("countdown").style.display = "none";
-            document.getElementById("content").style.display = "block";
-            clearInterval(x);
-            beep('3000', '900', '1', 'sine');
-          }
-          //seconds
-        }, 0)
-
-	console.log(inhale1, exhale1, inhale2, exhale2, inhale3, exhale3, hipopresive, totalTimer)
-}
-
-function stopWatch() { 
-	if (timer) { 
-	  setTimeout(stopWatch, 1000);
-    console.log("Something")
-	} 
-}
-
-let startBtn = document.getElementById('start'); 
-let stopBtn = document.getElementById('stop'); 
-let resetBtn = document.getElementById('reset');
-
-let inhale1 = 0;
-let exhale1 = 0;
-let inhale2 = 0;
-let exhale2 = 0;
-let inhale3 = 0;
-let exhale3 = 0;
-let hipopresive = 0;
-let totalTimer = 0;
-  
-stopBtn.addEventListener("click", function () { 
-    timer = false; 
-});  
-
-resetBtn.addEventListener('click', function () { 
-  timer = false; 
-  inhale1 = 0;
-  exhale1 = 0;
-  inhale2 = 0;
-  exhale2 = 0;
-  inhale3 = 0;
-  exhale3 = 0;
-  hipopresive = 0;
-  totalTimer = 0;
-  beep();
-}); 
-
-startBtn.addEventListener('click', function () { 
-    timer = true;
-    beep();
-    stopWatch(); 
-});
-
-
+// beep('700', '1000', '0.8', 'sine');
+//beep('500', '2000', '0.8', 'sine');
 //if you have another AudioContext class use that one, as some browsers have a limit
 var audioCtx = new (window.AudioContext || window.webkitAudioContext || window.audioContext);
 
@@ -174,4 +26,85 @@ function beep(duration, frequency, volume, type) {
 
 function addMinutes(date, minutes) {
   return new Date(date.getTime() + minutes*60000);
+}
+
+var globalTimer = 0;
+var timer, secondsLeft;
+var started = false;
+var repetion = 0;
+var nextSound = 0;
+var inhaleExhale = [0,0];
+let hipopresive = 0;
+
+function getGlobalTimer(){
+  const minToSecond = 60;
+	timer = document.getElementById("timer").value;
+  if (timer == null || timer.length == 0)
+    timer = 0;
+  return parseInt(timer) * minToSecond;
+}
+
+function starttimer(){
+  if (!started){
+    secondsLeft = getGlobalTimer();
+    getInformation();
+    started = true;
+    repetion = 0;
+    nextSound = secondsLeft - Number(inhaleExhale[repetion++ % 2]);
+    beep();
+  }
+  countdown();
+}
+
+function getInformation(){
+  inhaleExhale[0] = document.getElementById("inhale").value;
+  inhaleExhale[1] = document.getElementById("exhale").value;
+  hipopresive = document.getElementById("hipopresive").value;
+
+}
+
+function cddisplay() {
+  const minute = 60;
+
+  document.getElementById("minutes").innerText = Math.floor(secondsLeft / minute),
+    document.getElementById("seconds").innerText = Math.floor(secondsLeft % minute);
+}
+
+function countdown() {
+    cddisplay();
+
+    if (secondsLeft == nextSound)
+    {
+      if (repetion == 6){
+        nextSound = secondsLeft - Number(hipopresive);
+        repetion = 0;
+        console.log("Hipopressive timer");
+      }
+      else{
+        nextSound = secondsLeft - Number(inhaleExhale[repetion++ % 2]);
+        console.log("Setting next timer ", repetion);
+      }
+      console.log("Next beep in ", nextSound)
+      console.log("Beep-----------------");
+      beep();
+    }
+
+
+    if (secondsLeft === 0) {
+        cdpause()
+    } else {
+      secondsLeft--;
+      timer = setTimeout(countdown, 1000);
+    }
+}
+
+function cdpause() {
+    clearTimeout(timer);
+}
+
+function cdreset() {
+    cdpause();
+    secondsLeft = getGlobalTimer();
+    cddisplay();
+    started = false;
 }
